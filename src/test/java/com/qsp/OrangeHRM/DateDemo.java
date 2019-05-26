3672b88f8301b4c820bbcf8ba1b5e92046837c0f
Aravinda <aru03.info@gmail.com>
2019-05-26 16:23:31 +0530

    LoginTest

---
+++ src/test/java/com/qsp/OrangeHRM/DateDemo.java
@@ -0,0 +1,16 @@
+package com.qsp.OrangeHRM;
+
+import com.qsp.OrangeHRM.utils.Utility;
+
+
+public class DateDemo {
+
+	
+	public static void main(String[] args) {
+		Utility.getDate();
+	}
+
+	
+	
+	
+}
